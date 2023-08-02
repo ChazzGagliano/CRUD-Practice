138fa8645167942f9f4f07cdd9c38e338af7bb07
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-08-02 17:09:09 -0400

    create show requestn

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,8 @@
 import { PhotosIndex } from "./PhotosIndex";
 import { useState, useEffect } from "react";
 import { PhotosNew } from "./PhotosNew";
+import { PhotosShow } from "./PhotosShow"
+import { Modal } from "./Modal"
 
 export function Content() {
     const [photos, setPhotos] = useState([]);
@@ -11,18 +13,32 @@ export function Content() {
             setPhotos(response.data);
         })
     }
-    const handleCreatePhotos = (params, sucessCallback) => {
-        console.log("handleCreatePhoto", params)
-        axios.post("http://localhost:3000/photos.json", params).then((response) => {
-            setPhotos([...photos, response.data])
-            successCallbackj()
-        })
-    }
+    const handleCreatePhoto = (params, successCallback) => {
+             console.log("handleCreatePhoto", params);
+             axios.post("http://localhost:3000/photos.json", params).then((response) => {
+               setPhotos([...photos, response.data]);
+               successCallback();
+             });
+           };
 
+    const handleShowPhoto = (photo) => {
+        console.log("handleshowPhoto", photo),
+        setIsPhotoShowVisible(true);
+        setCurrentPhoto(photo)
+    }
+    const handleclose = () => {
+        console.log("handleClose")
+        setIsShowVisible(false)
+    }
     useEffect(handleIndexPhotos, []);
     
     return (
-        <PhotosNew onCreatePhoto={handleCreatePhoto} />
+        <>
         <PhotosIndex photos={photos} />
-    )
-  }
+        <PhotosNew onCreatePhoto={handleCreatePhoto} onShowPhoto={handleShowPhoto}/>
+        <Modal show={isPhotosShowVisible} onClose={handleClose}>
+            <PhotosShow photo={currentPhoto} />
+        </Modal>
+        </>
+        )
+    }
