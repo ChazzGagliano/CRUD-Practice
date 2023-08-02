07931e03e0989a8a4c1f16c106fc88c6596bbf63
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-08-02 16:49:21 -0400

    create New action

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,6 @@
 import { PhotosIndex } from "./PhotosIndex";
 import { useState, useEffect } from "react";
+import { PhotosNew } from "./PhotosNew";
 
 export function Content() {
     const [photos, setPhotos] = useState([]);
@@ -10,12 +11,18 @@ export function Content() {
             setPhotos(response.data);
         })
     }
+    const handleCreatePhotos = (params, sucessCallback) => {
+        console.log("handleCreatePhoto", params)
+        axios.post("http://localhost:3000/photos.json", params).then((response) => {
+            setPhotos([...photos, response.data])
+            successCallbackj()
+        })
+    }
 
     useEffect(handleIndexPhotos, []);
     
-    
-    
     return (
-      <PhotosIndex photos={photos}/>
+        <PhotosNew onCreatePhoto={handleCreatePhoto} />
+        <PhotosIndex photos={photos} />
     )
   }
