a339e0295f1270a5a2dd16ec88e97957d4abef77
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-08-04 16:04:42 -0400

    create index request

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,54 +1,22 @@
-import { PhotosIndex } from "./PhotosIndex";
-import { useState, useEffect } from "react";
-import { PhotosNew } from "./PhotosNew";
-import { PhotosShow } from "./PhotosShow"
-import { Modal } from "./Modal"
+import { PhotosIndex } from "./PhotosIndex"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 export function Content() {
-    const [photos, setPhotos] = useState([]);
 
-    const handleIndexPhotos = () => {
-        console.log("http://localhost:3000/photos.json").then((response) => {
-            console.log(response.data);
-            setPhotos(response.data);
-        })
-    }
-    const handleCreatePhoto = (params, successCallback) => {
-             console.log("handleCreatePhoto", params);
-             axios.post("http://localhost:3000/photos.json", params).then((response) => {
-               setPhotos([...photos, response.data]);
-               successCallback();
-             });
-           };
-
-    const handleShowPhoto = (photo) => {
-        console.log("handleshowPhoto", photo),
-        setIsPhotoShowVisible(true);
-        setCurrentPhoto(photo)
-    }
-    const handleclose = () => {
-        console.log("handleClose")
-        setIsShowVisible(false)
-    }
-
-    const handleDestroyPhoto = (photo) => {
-        console.log("handleDestroyPhoto", player);
-        axios.delete{'http://localhost:3000/photos/${photo.id}.json'}.then((response) => {
-            setPhotos(photos.filter((p) => p.id !== photo.id))
-            handleClose()
-        })
-
-    }
-    useEffect(handleIndexPhotos, []);
-
-    
+const [photos, setPhotos] = useState([])
+const handleIndexPhotos = () => {
+    console.log("handleIndexPhotos")
+    axios.get("http://localhost:3000/photos.json").then((response) => {
+        console.log(response.data)
+        setPhotos(response.data)
+    })
+}
+ 
+useEffect(handleIndexPhotos, [])
     return (
-        <>
-        <PhotosIndex photos={photos} />
-        <PhotosNew onCreatePhoto={handleCreatePhoto} onShowPhoto={handleShowPhoto}/>
-        <Modal show={isPhotosShowVisible} onClose={handleClose}>
-            <PhotosShow photo={currentPhoto} onDestroyPhoto={handleDestroyPhoto}/>
-        </Modal>
-        </>
-        )
-    }
+        <div>
+            <PhotosIndex photos={photos} />
+        </div>
+    )
+}
--- src/PhotosIndex.jsx
+++ src/PhotosIndex.jsx
@@ -1,16 +1,13 @@
-export function PhotosIndex(props) {
-    return (
-        <div>
-        <h1>All Photos</h1>
+export function PhotosIndex (props) {
+    <div>
+        <h1>All Photos </h1>
         {props.photos.map((photo) => (
             <div key={photo.id}>
                 <h2>{photo.name}</h2>
                 <img src={photo.url} />
                 <p>Width: {photo.width}</p>
                 <p>Height: {photo.height}</p>
-                <button onClick={() => props.onShowPhoto(photo)}>More info</button>
-            </div>    
+            </div>
         ))}
     </div>
-    );
 }
