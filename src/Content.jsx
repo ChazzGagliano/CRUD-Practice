066f931728e983ca658b377667c4b6023301dfce
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-08-07 19:20:08 -0400

    create index request

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,25 +1,23 @@
-import PhotosIndex from "./PhotosIndex"
 import axios from "axios"
 import {useState, useEffect} from "react"
+import PhotosIndex from "./PhotosIndex"
 
 
 export function Content() {
-    const photos = [
-        {id: 1, url: "https://via.placeholder.com/150", width: 300, height: 400}
-    ]
+    const [photos, setPhotos] = useState([])
+
     const handleIndexPhotos = () => {
         console.log("handleIndexPhotos")
-        axios.get("http://localhost:3000/photos.json").then((respose) => {
-            console.log(response.data)
+        axios.get("http://localhost:3000/photos.json").then((response) => {
             setPhotos(response.data)
+            console.log(response.data)
         })
     }
-
     useEffect(handleIndexPhotos, [])
-    
+
     return (
         <div>
             <PhotosIndex photos={photos}/>
         </div>
-        )
+    )
 }
