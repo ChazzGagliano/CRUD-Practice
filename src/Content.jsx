7c39219d02dcd674a8f1780157a2b3ee043eb91a
Chazz Gagliano <chazzgagliano@Chazzs-MacBook-Air.local>
2023-08-07 18:44:40 -0400

    create index request

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,70 +1,24 @@
 import { PhotosIndex } from "./PhotosIndex"
-import { PhotosNew } from "./PhotosNew"
-import { Modal } from "./Modal"
-import { PhotosShow } from "./PhotosShow"
 import axios from "axios"
 import { useState, useEffect } from "react"
 
-
 export function Content() {
+   
+    const [photos, setPhotos] = useState([])
+    const handleIndexPhotos = () => {
+        console.log("handleIndexPhotos")
+        axios.get("http://localhost:3000/photos.json").then((response) => {
+            console.log(response.data)
+            setPhotos(response.data)
+        })
+    }
 
-const [photos, setPhotos] = useState([])
-const [isPhotosShowVisible, setIsPhotosShowVisible] = useState(false)
-const [currentPhoto, setCurrentPhoto] = useState({})
-const handleIndexPhotos = () => {
-    console.log("handleIndexPhotos")
-    axios.get("http://localhost:3000/photos.json").then((response) => {
-        console.log(response.data)
-        setPhotos(response.data)
-    })
-}
-const handleCreatePhotos = (params, successCallback) => {
-    console.log("handleCreatePhotos")
-    axios.post("http://localhost:3000/photos.json", params).then((response) => {
-        setPhotos([...photos, response.data])
-        successCallback()
-    })
-}
-const handleShowPhoto = (photo) => {
-    console.log("handleShowPhoto", photo)
-    setIsPhotosShowVisible(true)
-    setCurrentPhoto(photo)
-}
-const handleUpdatePhoto = (id, params, successCallback) => {
-    console.log("handleUpdatePhoto")
-    axios.patch("http://localhost:3000/photos/${id}.json", params).then((response) => {
-        setPhotos(
-            photos.map((photos) => {
-                if (photo.id === response.data.id) {
-                    return response.data
-                } else {
-                    return photo
-                }
+    useEffect(handleIndexPhotos, [])
 
-            })
-        )
-        successCallback()
-        handleClose()
-    })
-}
-const handleClose = () => {
-    console.log("handleClose")
-    setIsPhotosShowVisible(false)
-}
-const handleDestroyPhoto = (photo) => {
-    console.log("handleDestroyPhoto")
-    axios.delete("http://localhost:3000/photos/${photo.id}.json")
-    setPhotos(photo.filter((p) => p.id !== photo.id))
-}
- 
-useEffect(handleIndexPhotos, [])
     return (
         <div>
-            <PhotosNew onCreatePhoto={handleCreatePhoto} />
-            <PhotosIndex photos={photos} onShowPhoto={handleShowPhoto} />
-            <Modal show={isPhotoShowVisible} onClose={handleClose}>
-                <PhotosShow photo={currentPhoto} onUpdatePhoto={handleUpdatePhoto} ondDestroyPhoto={handleDestroyPhoto} />
-            </Modal>
+            <PhotosIndex photos={photos} />
         </div>
-    )
+        )
+   
 }
